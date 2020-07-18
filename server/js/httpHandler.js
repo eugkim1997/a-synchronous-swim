127f22325f663f32581694eff79d7be2e4e3d3d2
Eugene Kim <eugkim1997@gmail.com>
2020-07-18 11:01:42 -0700

    server responds to get request

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -2,18 +2,31 @@ const fs = require('fs');
 const path = require('path');
 const headers = require('./cors');
 const multipart = require('./multipartUtils');
+const messageQueue = require('./messageQueue');
 
 // Path for the background image ///////////////////////
 module.exports.backgroundImageFile = path.join('.', 'background.jpg');
 ////////////////////////////////////////////////////////
 
-let messageQueue = null;
+//let messageQueue = null;
 module.exports.initialize = (queue) => {
   messageQueue = queue;
 };
 
 module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
+
+  if (req.method === 'GET') {
+    if (req.url === 'background') {
+
+    } else {
+      var commands = ['up', 'down', 'right', 'left'];
+      var randomCommand = commands[Math.floor(Math.random() * 3)];
+      res.writeHead(200, headers);
+      res.end(randomCommand);
+
+    }
+  }
   res.writeHead(200, headers);
   res.end();
   next(); // invoke next() at the end of a request to help with testing!
--- server/js/keypressHandler.js
+++ server/js/keypressHandler.js
@@ -1,5 +1,6 @@
 const _ = require('underscore');
 const keypress = require('keypress');
+const messageQueue = require('./messageQueue');
 
 ///////////////////////////////////////////////////////////////////////////////
 // Utility Function ///////////////////////////////////////////////////////////
@@ -38,9 +39,10 @@ module.exports.initialize = (callback) => {
     // check to see if the keypress itself is a valid message
     if (isValidMessage(key.name)) {
       callback(key.name);
+      messages.enqueue(key.name);
       return; // don't do any more processing on this key
     }
-    
+
     // otherwise build up a message from individual characters
     if (key && (key.name === 'return' || key.name === 'enter')) {
       // on enter, process the message
